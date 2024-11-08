fe1b506ef96cba70cab317c8f960fc01d0e9156a
GitHub Action <action@github.com>
2024-11-08 20:37:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/byshy/flutterassistant/services/MyProjectService.kt
+++ src/main/kotlin/com/github/byshy/flutterassistant/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.byshy.flutterassistant.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.byshy.flutterassistant.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
